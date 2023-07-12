954d19c162f677ffb191c61d50eafb77217346d3
Hayden Walker <haydenwalker@live.com>
2023-07-12 18:15:10 -0300

    Added to-do list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {Button, Container, FormGroup, List, ListItemButton, ListItemText, TextField, Typography, ThemeProvider, createTheme } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
+/**
+ * store a task
+ */
+class Task {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
 
 function App() {
+  const [tasks, setTasks] = useState(Array(0));
+  const [newTask, setNewTask] = useState("");
+
+  const handleClick = () => {
+    if(newTask !== "") {
+      setTasks(tasks.concat([new Task(newTask)])); 
+      setNewTask("")
+    }
+  }
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container id="app">      
+        <Typography variant="h1">to-do list</Typography>
+
+        <FormGroup id="entry">
+          <TextField autoComplete={false} label="Task" value={newTask} onChange={(event) => setNewTask(event.target.value)}/> 
+          <Button type="submit" variant="contained" onClick={handleClick}>
+            Add
+          </Button>
+        </FormGroup>
+
+        <Typography variant="body1">Tasks remaining (click to remove):</Typography>
+
+        <List>
+          {tasks.map(task => 
+            <ListItemButton onClick={() => setTasks(tasks.filter(item => !Object.is(item, task)))}>
+              <ListItemText class="task" primary={task.name} />
+            </ListItemButton>
+          )}
+        </List>
+      </Container>
+    </ThemeProvider>
   );
 }
 
